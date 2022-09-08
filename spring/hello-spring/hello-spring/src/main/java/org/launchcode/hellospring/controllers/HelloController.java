9c0e8e5388b34a2905242de72e1eb85429b415f7
Katlyn McDonald <kkmcoding@gmail.com>
2022-09-08 06:19:34 -0400

    Chapter 10/forms

---
+++ spring/hello-spring/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,47 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    // lives at /hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    // handles request of the form /hello?name=ANY
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // handles request of the form /hello/ANY
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // lives at /hello/form
+    @GetMapping("form")
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action = 'hello' method = 'post' >" +
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet me!' >" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+}
