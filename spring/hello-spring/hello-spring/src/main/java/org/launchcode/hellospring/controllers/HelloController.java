66090698e73a74bb662615b0dcb705b3d76b4f89
Katlyn McDonald <kkmcoding@gmail.com>
2022-09-08 20:19:58 -0400

    chap 10 exercises done

--- spring/hello-spring/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ spring/hello-spring/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -39,9 +39,44 @@ public class HelloController {
                 "<body>" +
                 "<form action = 'hello' method = 'post' >" +
                 "<input type = 'text' name = 'name' >" +
+                "<label for=\"lang-select\">Choose a language:</label>\n" +
+                "\n" +
+                "<select name=\"languages\" id=\"lang-select\">\n" +
+                "    <option value=\"\">---</option>\n" +
+                "    <option value=\"english\">English</option>\n" +
+                "    <option value=\"french\">French</option>\n" +
+                "    <option value=\"spanish\">Spanish</option>\n" +
+                "    <option value=\"korean\">Korean</option>\n" +
+                "    <option value=\"japanese\">Japanese</option>\n" +
+                "</select>\n" +
                 "<input type = 'submit' value = 'Greet me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    public String helloPost(@RequestParam String name, @RequestParam String languages) {
+        if (name == null) {
+            name = "World";
+        } else if (name.equals("")) {
+            name = "World";
+        }
+        return createMessage(name, languages);
+    }
+    public static String createMessage(String name, String lang) {
+        String greeting = "";
+        if (lang.equals("english")) {
+            greeting = "Hello, ";
+        } else if (lang.equals("french")) {
+            greeting = "Bonjour, ";
+        } else if (lang.equals("spanish")) {
+            greeting = "Hola, ";
+        } else if (lang.equals("korean")) {
+            greeting = "Annyeong haseyo, ";
+        } else if (lang.equals("japanese")) {
+            greeting = "Konnichiwa, ";
+        }
+        return greeting + name;
     }
 }
