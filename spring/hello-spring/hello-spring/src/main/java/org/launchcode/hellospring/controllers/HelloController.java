75369da21a8482a357968d099fe3bcf30066926d
Katlyn McDonald <kkmcoding@gmail.com>
2022-09-13 00:43:28 -0400

    video 13.5.2 done

--- spring/hello-spring/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ spring/hello-spring/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,82 +1,54 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris Bay
+ */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-
-    // lives at /hello/goodbye
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
-    // handles request of the form /hello?name=ANY
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
-    // handles request of the form /hello/ANY
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String agreeting =  "Hello, " + name + "!";
+        model.addAttribute("greeting", agreeting);
+        return "hello";
     }
 
-    // lives at /hello/form
+    // /hello/form
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action = 'hello' method = 'post' >" +
-                "<input type = 'text' name = 'name' >" +
-                "<label for=\"lang-select\">Choose a language:</label>\n" +
-                "\n" +
-                "<select name=\"languages\" id=\"lang-select\">\n" +
-                "    <option value=\"\">---</option>\n" +
-                "    <option value=\"english\">English</option>\n" +
-                "    <option value=\"french\">French</option>\n" +
-                "    <option value=\"spanish\">Spanish</option>\n" +
-                "    <option value=\"korean\">Korean</option>\n" +
-                "    <option value=\"japanese\">Japanese</option>\n" +
-                "</select>\n" +
-                "<input type = 'submit' value = 'Greet me!' >" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
-    @RequestMapping(value = "hello", method = RequestMethod.POST)
-    public String helloPost(@RequestParam String name, @RequestParam String languages) {
-        if (name == null) {
-            name = "World";
-        } else if (name.equals("")) {
-            name = "World";
-        }
-        return createMessage(name, languages);
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Katlyn");
+        names.add("Jon");
+        names.add("Spooky");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
-    public static String createMessage(String name, String lang) {
-        String greeting = "";
-        if (lang.equals("english")) {
-            greeting = "Hello, ";
-        } else if (lang.equals("french")) {
-            greeting = "Bonjour, ";
-        } else if (lang.equals("spanish")) {
-            greeting = "Hola, ";
-        } else if (lang.equals("korean")) {
-            greeting = "Annyeong haseyo, ";
-        } else if (lang.equals("japanese")) {
-            greeting = "Konnichiwa, ";
-        }
-        return greeting + name;
-    }
+
 }
